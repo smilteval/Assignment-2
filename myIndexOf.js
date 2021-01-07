047458f148aa77e280a09619f091f53912b2725d
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-06 21:07:50 -0500

    solved and tested IndexOf

---
+++ myIndexOf.js
@@ -0,0 +1,27 @@
+//returns the first index of 3 in an array
+
+const arr1 = [2, 5, 3, 6, 9];
+
+console.log("\nmySome\n")
+
+function findThree(value){
+  if(value === 3){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+function myIndexOf(arr, callback){
+  let index = -1;
+  for(let i = 0; i < arr.length; i++){
+    if(callback(arr[i])){
+        index = i;
+        break;
+    }
+  }
+  return index;
+}
+
+console.log(myIndexOf(arr1,findThree))
